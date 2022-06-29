f4580119913144836efa8a4df2c9495afa831e80
Yetife <tifetreats21@gmail.com>
2022-06-29 14:01:12 +0100

    Added files

--- controllers/flightController.js
+++ controllers/flightController.js
@@ -1,14 +1,21 @@
 const {Flights} = require('../models/Flight')
-const uuid = require("uuid").v4()
+const {v4: uuid} = require("uuid")
 
 exports.addFLight = async (req, res) => {
     try {
-        const flight = await req.body;
-        flight.id = uuid
-        Flights.push(flight);
+        const {title, time, price, date} = await req.body;
+        const newFlight = {
+                id: uuid(),
+                title,
+                time,
+                price,
+                date
+            }
+        // flight.id = uuid
+        Flights.push(newFlight);
         res.status(201).json({
             message: "Flight booked",
-            flights: flight
+            flights: newFlight
         })
     }catch (err) {
         res.status(500).json({message: err.message});
@@ -42,6 +49,34 @@ exports.getFlightById = async (req, res)=> {
     }
 }
 
-exports.deleteFlight = (req, res, next) => {
+exports.updateFlight = async (req, res) => {
+    try {
+        let id =req.params.id;
+        const flight = await Flights.find((flight) => flight.id === id);
+        const {title, time, price, date} = await req.body;
+        flight.title = title;
+        flight.time = time;
+        flight.price = price;
+        flight.date = date;
+        res.status(200).json({
+            message: "flight updated",
+            flight
+        })
+    } catch (err) {
+        res.status(500).json(err);
+    }
+}
 
+exports.deleteFlight = async (req, res) => {
+    try {
+        let id = req.params.id;
+        const flight = await Flights.find((flight) => flight.id === id);
+        Flights.splice(Flights.indexOf((flight), 1))
+        res.status(200).json({
+            message: "flight deleted",
+            flight
+        })
+    } catch (err) {
+        res.status(404).json({message: err.message})
+    }
 }
